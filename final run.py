f75f524495ccd1792b2c3a60592dab727a0674d5
Linjing <linjing.fiona@gmail.com>
2020-03-20 20:37:33 +0100

    Add files via upload

---
+++ final run.py
@@ -0,0 +1,520 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 23 2015, 02:52:03) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+["'", 'When', "I'M", 'a', 'Duchess', ',', "'", 'she', 'said', 'to', 'herself', ',', '(', 'not', 'in', 'a', 'very', 'hopeful', 'tone', 'though', ')', ',', "'", 'I', "won't", 'have', 'any', 'pepper', 'in', 'my', 'kitchen', 'AT', 'ALL', '.', 'Soup', 'does', 'very', 'well', 'without--Maybe', "it's", 'always', 'pepper', 'that', 'makes', 'people', 'hot-tempered', ',', "'", '.', '.', '.', 'That', 'U.S.A.', "i'm", 'poster-print', 'costs', '12.4', '10.01', '0.923', '.', '.', '.', '2.1', '3.14', '12.22', '444.444']
+1 : Dutch
+2 : Complex
+11 : special
+2 : hard
+2 : Errors
+37 : is
+6 : right
+5 : preferably
+1 : Readability
+11 : break
+9 : Unless
+6 : it
+3 : Beautiful
+6 : obvious
+22 : the
+6 : beats
+13 : Although
+7 : Namespaces
+3 : explain
+3 : implementation
+1 : first
+34 : better
+4 : Now
+3 : ugly
+11 : aren't
+5 : only
+4 : guess
+1 : you're
+6 : often
+1 : not
+5 : one--
+7 : ,
+6 : practicality
+11 : enough
+5 : There
+11 : rules
+2 : may
+6 : now
+7 : those
+1 : Explicit
+6 : Simple
+2 : pass
+1 : good
+4 : temptation
+4 : refuse
+1 : at
+12 : one
+12 : never
+3 : If
+7 : nested
+12 : do
+6 : way
+1 : unless
+2 : bad
+6 : complex
+2 : it's
+23 : to
+1 : that
+7 : let's
+10 : idea
+1 : counts
+11 : of
+34 : than
+9 : silenced
+76 : .
+1 : implicit
+4 : ambiguity
+24 : -
+7 : great
+5 : Sparse
+11 : Special
+2 : complicated
+5 : and
+7 : are
+4 : face
+12 : *
+7 : more
+7 : honking
+9 : explicitly
+3 : a
+7 : should
+7 : be
+4 : In
+7 : Flat
+6 : purity
+5 : dense
+7 : !
+1 : easy
+11 : cases
+2 : silently
+enter the filename:6996RG.txt.utf-8.txt
+average word length is: 3.928264950388844
+type/token ratio is: 0.19881335478680612
+>>> ================================ RESTART ================================
+>>> 
+number of syllables: During 2
+number of syllables: the 1
+number of syllables: whole 2
+number of syllables: of 1
+number of syllables: a 1
+number of syllables: dull, 1
+number of syllables: dark, 1
+number of syllables: and 1
+number of syllables: soundless 2
+number of syllables: day 1
+number of syllables: in 1
+number of syllables: the 1
+number of syllables: autumn 2
+number of syllables: of 1
+number of syllables: the 1
+number of syllables: year, 1
+number of syllables: when 1
+number of syllables: the 1
+number of syllables: clouds 1
+number of syllables: hung 1
+number of syllables: oppressively 4
+number of syllables: low 1
+number of syllables: in 1
+number of syllables: the 1
+number of syllables: heavens, 2
+number of syllables: I 1
+number of syllables: had 1
+number of syllables: been 1
+number of syllables: passing 2
+number of syllables: alone, 3
+number of syllables: on 1
+number of syllables: horseback, 3
+number of syllables: through 1
+number of syllables: a 1
+number of syllables: singularly 3
+number of syllables: dreary 1
+number of syllables: tract 1
+number of syllables: of 1
+number of syllables: country, 1
+number of syllables: and 1
+number of syllables: at 1
+number of syllables: length 1
+number of syllables: found 1
+number of syllables: myself, 1
+number of syllables: as 1
+number of syllables: the 1
+number of syllables: shades 2
+number of syllables: of 1
+number of syllables: the 1
+number of syllables: evening 3
+number of syllables: drew 1
+number of syllables: on, 1
+number of syllables: within 2
+number of syllables: view 1
+number of syllables: of 1
+number of syllables: the 1
+number of syllables: melancholy 3
+number of syllables: House 2
+number of syllables: of 1
+number of syllables: Usher. 2
+number of syllables: I 1
+number of syllables: know 1
+number of syllables: not 1
+number of syllables: how 1
+number of syllables: it 1
+number of syllables: was 1
+number of syllables: -- 0
+number of syllables: but, 1
+number of syllables: with 1
+number of syllables: the 1
+number of syllables: first 1
+number of syllables: glimpse 2
+number of syllables: of 1
+number of syllables: the 1
+number of syllables: building, 2
+number of syllables: a 1
+number of syllables: sense 2
+number of syllables: of 1
+number of syllables: insufferable 5
+number of syllables: gloom 1
+number of syllables: pervaded 3
+number of syllables: my 0
+number of syllables: spirit. 2
+number of syllables: I 1
+number of syllables: say 1
+number of syllables: insufferable; 5
+number of syllables: for 1
+number of syllables: the 1
+number of syllables: feeling 2
+number of syllables: was 1
+number of syllables: unrelieved 4
+number of syllables: by 0
+number of syllables: any 1
+number of syllables: of 1
+number of syllables: that 1
+number of syllables: half-pleasurable, 5
+number of syllables: because 3
+number of syllables: poetic, 2
+number of syllables: sentiment, 3
+number of syllables: with 1
+number of syllables: which 1
+number of syllables: the 1
+number of syllables: mind 1
+number of syllables: usually 2
+number of syllables: receives 3
+number of syllables: even 2
+number of syllables: the 1
+number of syllables: sternest 2
+number of syllables: natural 3
+number of syllables: images 3
+number of syllables: of 1
+number of syllables: the 1
+number of syllables: desolate 4
+number of syllables: or 1
+number of syllables: terrible. 3
+number of syllables: I 1
+number of syllables: looked 2
+number of syllables: upon 2
+number of syllables: the 1
+number of syllables: scene 2
+number of syllables: before 3
+number of syllables: me 1
+number of syllables: -- 0
+number of syllables: upon 2
+number of syllables: the 1
+number of syllables: mere 2
+number of syllables: house, 2
+number of syllables: and 1
+number of syllables: the 1
+number of syllables: simple 2
+number of syllables: landscape 3
+number of syllables: features 3
+number of syllables: of 1
+number of syllables: the 1
+number of syllables: domain 2
+number of syllables: -- 0
+number of syllables: upon 2
+number of syllables: the 1
+number of syllables: bleak 1
+number of syllables: walls 1
+number of syllables: -- 0
+number of syllables: upon 2
+number of syllables: the 1
+number of syllables: vacant 2
+number of syllables: eye-like 4
+number of syllables: windows 2
+number of syllables: -- 0
+number of syllables: upon 2
+number of syllables: a 1
+number of syllables: few 1
+number of syllables: rank 1
+number of syllables: sedges 2
+number of syllables: -- 0
+number of syllables: and 1
+number of syllables: upon 2
+number of syllables: a 1
+number of syllables: few 1
+number of syllables: white 2
+number of syllables: trunks 1
+number of syllables: of 1
+number of syllables: decayed 3
+number of syllables: trees 1
+number of syllables: -- 0
+number of syllables: with 1
+number of syllables: an 1
+number of syllables: utter 2
+number of syllables: depression 3
+number of syllables: of 1
+number of syllables: soul 1
+number of syllables: which 1
+number of syllables: I 1
+number of syllables: can 1
+number of syllables: compare 3
+number of syllables: to 1
+number of syllables: no 1
+number of syllables: earthly 1
+number of syllables: sensation 3
+number of syllables: more 2
+number of syllables: properly 2
+number of syllables: than 1
+number of syllables: to 1
+number of syllables: the 1
+number of syllables: after-dream 3
+number of syllables: of 1
+number of syllables: the 1
+number of syllables: reveller 3
+number of syllables: upon 2
+number of syllables: opium 2
+number of syllables: -- 0
+number of syllables: the 1
+number of syllables: bitter 2
+number of syllables: lapse 2
+number of syllables: into 2
+number of syllables: every-day 3
+number of syllables: life 2
+number of syllables: -- 0
+number of syllables: the 1
+number of syllables: hideous 2
+number of syllables: dropping 2
+number of syllables: off 1
+number of syllables: of 1
+number of syllables: the 1
+number of syllables: veil. 1
+number of syllables: There 2
+number of syllables: was 1
+number of syllables: an 1
+number of syllables: iciness, 3
+number of syllables: a 1
+number of syllables: sinking, 2
+number of syllables: a 1
+number of syllables: sickening 3
+number of syllables: of 1
+number of syllables: the 1
+number of syllables: heart 1
+number of syllables: -- 0
+number of syllables: an 1
+number of syllables: unredeemed 4
+number of syllables: dreariness 3
+number of syllables: of 1
+number of syllables: thought 1
+number of syllables: which 1
+number of syllables: no 1
+number of syllables: goading 2
+number of syllables: of 1
+number of syllables: the 1
+number of syllables: imagination 5
+number of syllables: could 1
+number of syllables: torture 3
+number of syllables: into 2
+number of syllables: aught 1
+number of syllables: of 1
+number of syllables: the 1
+number of syllables: sublime. 3
+number of syllables: What 1
+number of syllables: was 1
+number of syllables: it 1
+number of syllables: -- 0
+number of syllables: I 1
+number of syllables: paused 2
+number of syllables: to 1
+number of syllables: think 1
+number of syllables: -- 0
+number of syllables: what 1
+number of syllables: was 1
+number of syllables: it 1
+number of syllables: that 1
+number of syllables: so 1
+number of syllables: unnerved 3
+number of syllables: me 1
+number of syllables: in 1
+number of syllables: the 1
+number of syllables: contemplation 4
+number of syllables: of 1
+number of syllables: the 1
+number of syllables: House 2
+number of syllables: of 1
+number of syllables: Usher? 2
+number of syllables: It 1
+number of syllables: was 1
+number of syllables: a 1
+number of syllables: mystery 1
+number of syllables: all 1
+number of syllables: insoluble; 4
+number of syllables: nor 1
+number of syllables: could 1
+number of syllables: I 1
+number of syllables: grapple 2
+number of syllables: with 1
+number of syllables: the 1
+number of syllables: shadowy 2
+number of syllables: fancies 2
+number of syllables: that 1
+number of syllables: crowded 2
+number of syllables: upon 2
+number of syllables: me 1
+number of syllables: as 1
+number of syllables: I 1
+number of syllables: pondered. 3
+number of syllables: I 1
+number of syllables: was 1
+number of syllables: forced 2
+number of syllables: to 1
+number of syllables: fall 1
+number of syllables: back 1
+number of syllables: upon 2
+number of syllables: the 1
+number of syllables: unsatisfactory 5
+number of syllables: conclusion, 3
+number of syllables: that 1
+number of syllables: while, 2
+number of syllables: beyond 2
+number of syllables: doubt, 1
+number of syllables: there 2
+number of syllables: are 2
+number of syllables: combinations 4
+number of syllables: of 1
+number of syllables: very 1
+number of syllables: simple 2
+number of syllables: natural 3
+number of syllables: objects 2
+number of syllables: which 1
+number of syllables: have 2
+number of syllables: the 1
+number of syllables: power 2
+number of syllables: of 1
+number of syllables: thus 1
+number of syllables: affecting 3
+number of syllables: us, 1
+number of syllables: still 1
+number of syllables: the 1
+number of syllables: analysis 3
+number of syllables: of 1
+number of syllables: this 1
+number of syllables: power 2
+number of syllables: lies 1
+number of syllables: among 2
+number of syllables: considerations 5
+number of syllables: beyond 2
+number of syllables: our 1
+number of syllables: depth. 1
+number of syllables: It 1
+number of syllables: was 1
+number of syllables: possible, 3
+number of syllables: I 1
+number of syllables: reflected, 3
+number of syllables: that 1
+number of syllables: a 1
+number of syllables: mere 2
+number of syllables: different 3
+number of syllables: arrangement 4
+number of syllables: of 1
+number of syllables: the 1
+number of syllables: particulars 4
+number of syllables: of 1
+number of syllables: the 1
+number of syllables: scene, 2
+number of syllables: of 1
+number of syllables: the 1
+number of syllables: details 2
+number of syllables: of 1
+number of syllables: the 1
+number of syllables: picture, 3
+number of syllables: would 1
+number of syllables: be 1
+number of syllables: sufficient 3
+number of syllables: to 1
+number of syllables: modify, 2
+number of syllables: or 1
+number of syllables: perhaps 2
+number of syllables: to 1
+number of syllables: annihilate 5
+number of syllables: its 1
+number of syllables: capacity 3
+number of syllables: for 1
+number of syllables: sorrowful 3
+number of syllables: impression; 3
+number of syllables: and, 1
+number of syllables: acting 2
+number of syllables: upon 2
+number of syllables: this 1
+number of syllables: idea, 2
+number of syllables: I 1
+number of syllables: reined 2
+number of syllables: my 0
+number of syllables: horse 2
+number of syllables: to 1
+number of syllables: the 1
+number of syllables: precipitous 4
+number of syllables: brink 1
+number of syllables: of 1
+number of syllables: a 1
+number of syllables: black 1
+number of syllables: and 1
+number of syllables: lurid 2
+number of syllables: tarn 1
+number of syllables: that 1
+number of syllables: lay 1
+number of syllables: in 1
+number of syllables: unruffled 3
+number of syllables: lustre 2
+number of syllables: by 0
+number of syllables: the 1
+number of syllables: dwelling, 2
+number of syllables: and 1
+number of syllables: gazed 2
+number of syllables: down 1
+number of syllables: -- 0
+number of syllables: but 1
+number of syllables: with 1
+number of syllables: a 1
+number of syllables: shudder 2
+number of syllables: even 2
+number of syllables: more 2
+number of syllables: thrilling 2
+number of syllables: than 1
+number of syllables: before 3
+number of syllables: -- 0
+number of syllables: upon 2
+number of syllables: the 1
+number of syllables: remodelled 4
+number of syllables: and 1
+number of syllables: inverted 3
+number of syllables: images 3
+number of syllables: of 1
+number of syllables: the 1
+number of syllables: gray 1
+number of syllables: sedge, 2
+number of syllables: and 1
+number of syllables: the 1
+number of syllables: ghastly 1
+number of syllables: tree-stems, 2
+number of syllables: and 1
+number of syllables: the 1
+number of syllables: vacant 2
+number of syllables: and 1
+number of syllables: eye-like 4
+number of syllables: windows. 2
+[('During', 'the'), ('the', 'whole'), ('whole', 'of'), ('of', 'a'), ('a', 'dull'), ('dull', 'dark'), ('dark', 'and'), ('and', 'soundless'), ('soundless', 'day'), ('day', 'in'), ('in', 'the'), ('the', 'autumn'), ('autumn', 'of'), ('of', 'the'), ('the', 'year'), ('year', 'when'), ('when', 'the'), ('the', 'clouds'), ('clouds', 'hung'), ('hung', 'oppressively'), ('oppressively', 'low'), ('low', 'in'), ('in', 'the'), ('the', 'heavens'), ('heavens', 'I'), ('I', 'had')]
+>>> 
